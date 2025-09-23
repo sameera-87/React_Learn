a1a119c96805372b8552861b73767888872f30f4
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-23 22:08:14 +0530

    Event handeler recap is finished

--- blue-print/src/main.jsx
+++ blue-print/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App(){
+
+  function handleClick() {
+    console.log("I was clicked!")
+  }
+
+  function handleMouseOver(){
+    console.log("I was hovered")
+  }
+
+  return (
+    <main className='container'>
+      <img
+        src="https://picsum.photos/640/360"
+        alt="Placeholder image from Picsum"
+        onMouseOver={handleMouseOver}
+      />
+      <button onClick={handleClick}>Click me</button>  
+    </main>
+  )
+
+}
+
+ReactDOM.createRoot(document
+  .getElementById('root')).render(<App />);
