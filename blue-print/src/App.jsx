52d62b93299ead0a61bb4a941ef2cfd4510742f8
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-24 19:25:24 +0530

    completed

--- blue-print/src/App.jsx
+++ blue-print/src/App.jsx
@@ -1,11 +1,17 @@
-import Header from "./Header"
-import Main from "./MainComponent"
+import React from "react"
 
 export default function App(){
+  let [isImportant, setIsImportant] = React.useState("Yes")
+  // setIsImportant("Heck Yess!")
+
+  function handleClick(){
+    setIsImportant("Definitely")
+  }
+
   return(
-    <>
-      <Header />
-      <Main />
-    </>
+    <main>
+      <h1 className="title">Is state important to know</h1>
+      <button onClick={handleClick} className="value">{isImportant}</button>
+    </main>
   )
 }
