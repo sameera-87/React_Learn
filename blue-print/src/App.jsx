6138e46394f2f441c5d1b7da0106d68a00853230
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-25 09:12:01 +0530

    Challenge completed, changing the bracnh

--- blue-print/src/App.jsx
+++ blue-print/src/App.jsx
@@ -1,11 +1,21 @@
-import Header from "./Header"
-import Main from "./MainComponent"
+import { useState } from "react"
 
 export default function App(){
-  return(
-    <>
-      <Header />
-      <Main />
-    </>
+
+  const [isGoingOut, setIsGoingOut] = useState(false)
+
+  function changeMind(){
+    setIsGoingOut(prev => !prev)
+  }
+
+  return (
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button 
+        onClick={changeMind} 
+        className="value"
+        aria-label={'Current answer is ${isGoingOut ? "Yes" : "No"}. Click to change it.'}
+        >{isGoingOut ? "Yes" : "No"}</button>
+    </main>
   )
 }
