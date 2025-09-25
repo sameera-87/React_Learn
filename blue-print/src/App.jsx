abd4218f5579e9a4d9136973d70e07f936db5e5b
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-25 09:46:58 +0530

    Challenge3 completed

--- blue-print/src/App.jsx
+++ blue-print/src/App.jsx
@@ -1,11 +1,33 @@
-import Header from "./Header"
-import Main from "./MainComponent"
+import React from "react"
 
 export default function App(){
+
+  const [myFavouriteThings, setMyFavouriteThings] = React.useState([])
+  const allFavouriteThings = [
+    "🎶", "😘", "😇", "🦖", "🐬", "🐧", 
+    "🚴‍♂️", "🎈", "🎇", "⚾", "🥎", "🏀",
+    "🏐", "🏈", "🏉", "🏏", "🎳", "🥌",
+    "⛳", "⛸️", "🎯", "🥊", "🏓", "🏑",
+    "🏸", "🎾", "🎤", "🎹", "📕", "📖"
+  ]
+
+  const thingsElements = myFavouriteThings.map(thing => <p key={thing}>{thing}</p>)
+
+  function addFavouriteThing(){
+    setMyFavouriteThings(
+      prevFavThings => [
+        ...prevFavThings,
+        allFavouriteThings[prevFavThings.length]
+      ]
+    )
+  }
+
   return(
-    <>
-      <Header />
-      <Main />
-    </>
+    <main>
+      <button onClick={addFavouriteThing}>Add item</button>
+      <section aria-live="police">
+        {thingsElements}
+      </section>
+    </main>
   )
 }
