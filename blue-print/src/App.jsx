8b3890633cd3f96dc69b41a81c0e008ba1f2aa82
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-26 15:35:19 +0530

    changes completed

--- blue-print/src/App.jsx
+++ blue-print/src/App.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 
 export default function App(){
-
   function signUp(formData){
+
+    const data = Object.fromEntries(formData)
+    const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+    const allData = {
+      ...data,
+      dietaryRestrictions
+    }
+
+    console.log(allData)
+
+
     // event.preventDefault()
     // // console.log("submitted")
     // const formEl = event.currentTarget
     // const formData = new FormData(formEl)
-    const email = formData.get("email")
-    const password = formData.get("password")
-    const employmentStatus = formData.get("employmentStatus")
-    console.log(employmentStatus)
+    // const email = formData.get("email")
+    // const password = formData.get("password")
+    // const employmentStatus = formData.get("employmentStatus")
+    // const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+    // const favColor = formData.get("favColor")
+    // console.log(favColor)
     // console.log(email,password)
     // formEl.reset()
   }
@@ -51,6 +63,37 @@ export default function App(){
         </fieldset>
 
         <br/>
+
+        <fieldset>
+          <legend>Dietary restrictions:</legend>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="kosher"/>
+            Kosher
+          </label>
+
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="chicken"/>
+            Chicken
+          </label>
+
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" defualtChecked={true} value="vegan"/>
+            Vegan
+          </label>
+        </fieldset>
+
+        <br/>
+
+        <label htmlFor='favColor'>What is your favourite color?</label>
+        <select id='favColor' name="favColor">
+          <option value="Red">Red</option>
+          <option value="Green">Green</option>
+          <option value="Blue">Blue</option>
+          <option value="Indigo">Indigo</option>
+          <option value="Purple">Purple</option>
+        </select>
+        <br/>
+
         <button>Submit</button>
       </form>
     </section>
