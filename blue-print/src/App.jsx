1f4de6e35cc49e737f90071112654e8a0ef253e7
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-26 10:07:50 +0530

    Form completed

--- blue-print/src/App.jsx
+++ blue-print/src/App.jsx
@@ -1,11 +1,33 @@
-import Header from "./Header"
-import Main from "./MainComponent"
+import React from 'react'
+import ReactDom from 'react-dom/client'
 
 export default function App(){
+
+  function signUp(formData){
+    // event.preventDefault()
+    // // console.log("submitted")
+    // const formEl = event.currentTarget
+    // const formData = new FormData(formEl)
+    const email = formData.get("email")
+    const password = formData.get("password")
+    // console.log(email,password)
+    // formEl.reset()
+  }
+
   return(
-    <>
-      <Header />
-      <Main />
-    </>
+    <section>
+      <h1>Signup From</h1>
+      <form action={signUp}>
+        <label htmlFor="email">Email : </label>
+        <input id="email" type="email" name="email" placeholder="Email" />
+        <br/>
+
+        <label htmlFor="password">Password : </label>
+        <input id="password" type="password" name="password"  />
+        <br/>
+
+        <button>Submit</button>
+      </form>
+    </section>
   )
 }
