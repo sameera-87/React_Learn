f7cb36c46348718ecc16167c2b1d07bae35f94f6
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-26 10:30:15 +0530

    Form implemented

--- blue-print/src/MainComponent.jsx
+++ blue-print/src/MainComponent.jsx
@@ -1,21 +1,27 @@
+import React from "react"
 
 export default function MainComponent(){
 
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = React.useState([])
 
-    const ingredientsListItems = ingredients.map(
-      ingredient => (
+    const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
       )  
     )
 
+    function addIngredient(formData){
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add Ingredient</button>
             </form>
