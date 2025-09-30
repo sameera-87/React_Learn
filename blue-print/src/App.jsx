9be35dbcba1aa7405dfb04612c5a6b9e5b1e6eb3
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-30 13:36:52 +0530

    Color changing mechanism implemented

--- blue-print/src/App.jsx
+++ blue-print/src/App.jsx
@@ -2,16 +2,30 @@ import React from "react"
 import padsData from "./pads"
 import Pad from "./Pad"
 
-export default function App({ darkMode}){
+export default function App(){
 
   const [pads, setPads] = React.useState(padsData)
 
-  const styles = {
-    backgroundColor: darkMode ? "#222222" : "#cccccc"
+  function turnAllPadsOff(){
+    console.log("Turning off")
+    setPads(prevPads => prevPads.map(pad => ({
+      ...pad,
+      on: false
+    })))
+  }
+
+  // const styles = {
+  //   backgroundColor: darkMode ? "#222222" : "#cccccc"
+  // }
+
+  function toggle(id){
+    setPads(prevPads => prevPads.map(item => {
+      return item.id == id ? {...item, on: !item.on} : item
+    }))
   }
 
   const buttonElements = pads.map(pad => (
-    <Pad key={pad.id} color={pad.color} on={pad.on}/>
+    <Pad toggle={toggle} id={pad.id} key={pad.id} color={pad.color} on={pad.on}/>
   ))
 
   return(
@@ -19,6 +33,7 @@ export default function App({ darkMode}){
       <div className="pad-container">
         {buttonElements}
       </div>
+      <button className="all-off" onClick={turnAllPadsOff}>Turn All Off</button>
     </main>
   )
 }
--- blue-print/src/Pad.jsx
+++ blue-print/src/Pad.jsx
@@ -2,17 +2,11 @@ import React from "react"
 
 export default function Pad(props){
 
-    const [on, setOn] = React.useState(props.on)
-
-    function toggle(){
-        setOn(prevOn => !prevOn)
-    }
-
     return (
         <button 
             style={{backgroundColor: props.color}}
-            className={on ? "on" : undefined}
-            onClick={toggle}
+            className={props.on ? "on" : undefined}
+            onClick={() => props.toggle(props.id)}
         ></button>
         
     )
