b9ca6b68ee2660adbd8ab35e1f7098368eb9f790
sameera-87 <sameekumarasinghe87@gmail.com>
2025-09-30 18:05:45 +0530

    Checf claude project completed. things learnt, 1. Event listners 2. state 3. Conditional rendering 4. Forms 5. State management strategies

--- blue-print/src/MainComponent.jsx
+++ blue-print/src/MainComponent.jsx
@@ -1,21 +1,18 @@
 import React from "react"
+import IngredientsList from "./components/IngredientList"
+import ClaudeRecipe from "./components/ClaudeRecipe"
+import { getRecipeFromMistral } from "./ai"
 
 export default function MainComponent(){
 
-    const [ingredients, setIngredients] = React.useState(
-        ["all the main spices", "pasta", "ground beef", "tomato paste"]
-    )
+    const [ingredients, setIngredients] = React.useState("")
        
-    const [recipeShown, setRecipeShown] = React.useState(false)
+    const [recipe, setRecipe] = React.useState(false)
 
-    function toggleRecipeShown(){
-        setRecipeShown(prevShown => !prevShown)
+    async function getRecipe(){
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
     }
-
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-      )  
-    )
 
     function addIngredient(formData){
         const newIngredient = formData.get("ingredient")
@@ -34,19 +31,13 @@ export default function MainComponent(){
                 <button>Add Ingredient</button>
             </form>     
 
-            {ingredients.length > 0 ? <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+            {ingredients.length > 0 && 
+                <IngredientsList ingredients={ingredients} 
+                getRecipe={getRecipe}                
+                />
+            }
 
-                {ingredients.length > 3 ? <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={toggleRecipeShown}>Get a recipe</button>
-                </div> : null}
-            </section> : null }
-            {recipeShown && <h3>The response resides here</h3>}        
+            {recipe && <ClaudeRecipe recipe={recipe}/>}        
             
         </main>
     )
