73c2eeb176693895c43fca6cdacece0e51f24ca7
sameera-87 <sameekumarasinghe87@gmail.com>
2025-10-02 09:13:36 +0530

    useEffects basics covered

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,11 +1,35 @@
-import Header from "./Header"
-import Main from "./MainComponent"
+import React from "react"
 
-export default function App(){
+export default function App(props){
+
+  const [starWarsData, setstarWarsData] = React.useState({})
+  const [count, setCount] = React.useState(1)
+
+  console.log("Rendered")
+
+  React.useEffect(() => {
+    console.log("Effect ran")
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then(res => res.json())
+      .then(data => setstarWarsData(data))
+  }, [count])
+ 
+  // React.useEffect(function() {
+  //   console.log("Effect ran")
+  //   fetch("https://swapi.dev/api/people/1")
+  //     .then(res => res.json())
+  //     // .then(data => setstarWarsData(data))
+  // }, [0])
+
+
   return(
-    <>
-      <Header />
-      <Main />
-    </>
+    <div>
+      <h2>The Count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Get next character</button>
+      <pre>{JSON.stringify(starWarsData , null, 2)}</pre>
+    </div>
   )
 }
+
+
+// 10.22 - stop
